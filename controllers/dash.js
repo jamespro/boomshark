e6b57483b6b2a3159e8265d46cf6da333c144045
jerednav <80506833+jerednav@users.noreply.github.com>
2021-04-29 14:55:48 -0700

    Update and rename todos.js to dash.js

--- controllers/dash.js
+++ controllers/dash.js
@@ -1,19 +1,21 @@
-const Todo = require('../models/dash')
+const Dash = require('../models/dash')
 
 module.exports = {
-    getTodos: async (req,res)=>{
+    //retrieve Posts
+    getPosts: async (req,res)=>{
         console.log(req.user)
         try{
-            const todoItems = await Todo.find({userId:req.user.id})
-            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            const todoItems = await Dash.find({userId:req.user.id})
+            const itemsLeft = await Dash.countDocuments({userId:req.user.id,completed: false})
+            res.render('dash.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
         }catch(err){
             console.log(err)
         }
     },
-    createTodo: async (req, res)=>{
+    //create Post, the action is found in .views/dash.ejs
+    createPost: async (req, res)=>{
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+            await Dash.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
             console.log('Todo has been added!')
             res.redirect('/todos')
         }catch(err){
@@ -22,7 +24,7 @@ module.exports = {
     },
     markComplete: async (req, res)=>{
         try{
-            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+            await Dash.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
                 completed: true
             })
             console.log('Marked Complete')
@@ -33,7 +35,7 @@ module.exports = {
     },
     markIncomplete: async (req, res)=>{
         try{
-            await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
+            await Dash.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
                 completed: false
             })
             console.log('Marked Incomplete')
@@ -45,7 +47,7 @@ module.exports = {
     deleteTodo: async (req, res)=>{
         console.log(req.body.todoIdFromJSFile)
         try{
-            await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
+            await Dash.findOneAndDelete({_id:req.body.todoIdFromJSFile})
             console.log('Deleted Todo')
             res.json('Deleted It')
         }catch(err){
