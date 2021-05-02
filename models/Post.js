b11a9f71bfcc5947b1d7f54100a89d2e0e970773
James Prickitt <jwprickitt@gmail.com>
2021-05-02 17:05:57 -0400

    #18 #16 Converted to use posts as routes, show individual post, update post fields

--- models/Post.js
+++ models/Post.js
@@ -1,22 +1,38 @@
 const mongoose = require('mongoose')
 
 const PostSchema = new mongoose.Schema({
-  post: {
+  title: {
     type: String,
     required: true,
   },
-  caption: {
+  image: {
     type: String,
     required: true
   },
-  image: {
+//   cloudinaryId: {
+//     type: String,
+//     require: true,
+//   },
+  caption: {
     type: String,
     required: true
   },
   link: {
     type: String,
     required: false
-  }
+  },
+//   likes: {
+//     type: Number,
+//     required: true,
+//   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 })
 
 module.exports = mongoose.model('Post', PostSchema)
