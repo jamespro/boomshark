bbf130c53540100a478636e7b620e2a1e4aa03e4
vinson-han <Han_vinson@student.smc.edu>
2021-05-03 22:34:16 -0700

    DB_String from database.js

--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-process.env.DB_STRING = "mongodb+srv://demo:test@cluster0.nvjw1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const connectDB = async () => {
   try {
