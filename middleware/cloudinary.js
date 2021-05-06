31da3b2f41db302e6af3c5fc1094d76e4f9e5b69
justytho <justinmthomp@gmail.com>
2021-05-06 00:43:32 -0400

    Added Cloudinary +  Configuration file

---
+++ middleware/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2v;
+
+cloudinary.config({
+    cloud_name: 'dhdwf9wqy',
+    api_key: '711859142549368',
+    api_secret: 'RBiljIeK471nsljZV8yX0G_oGA0'
+});
+
+module.exports = cloudinary;
