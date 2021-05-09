2638d7bf9f0ca560a7c88de8d5eadc5d366afbd6
James Prickitt <jwprickitt@gmail.com>
2021-05-08 21:32:13 -0400

    added cloudinary but I don't see them in cloudinary yet

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const upload = require("../middleware/multer");
 const postsController = require('../controllers/posts') 
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
