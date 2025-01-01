7021194831dfd519392c351ab69c58f0fdf5f386
James Prickitt <james@precision-websites.com>
2025-01-01 13:09:01 -0500

    Update handleCameraUpload
    Update handleCameraUpload because getting 500 server error because it's having a problem with "id". Sounds like it really wants a logged in user... but maybe for testing we can say it's optional. Maybe should have a default user for dev.

--- controllers/camera.js
+++ controllers/camera.js
@@ -4,6 +4,9 @@ const Post = require("../models/Post");
 module.exports = {
   handleCameraUpload: async (req, res) => {
     try {
+      console.log("Received upload request");
+      console.log("File:", req.file);
+      console.log("Body:", req.body);
       // Upload image to cloudinary
       const result = await cloudinary.uploader.upload(req.file.path);
 
@@ -13,14 +16,14 @@ module.exports = {
         cloudinaryId: result.public_id,
         caption: "Uploaded from EINAR camera",
         likes: 0,
-        user: req.user.id,
+        user: req.user ? req.user.id : null, // Add this check. TODO: Later, require a user to be logged in for security.
       });
 
       console.log("Camera image has been uploaded!");
       res.status(200).json({ message: "Upload successful" });
     } catch (err) {
-      console.log(err);
+      console.error("Upload error:", err);
       res.status(500).json({ error: "Error in upload" });
     }
-  }
+  },
 };
