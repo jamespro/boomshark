add26d1315faa687e4479e76a73f36636e1ca052
James Prickitt <james@precision-websites.com>
2025-01-01 20:29:24 -0500

    Layout changes

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,7 +6,7 @@ const Post = require('../models/Post')
 module.exports = {
     getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
+      const posts = await Post.find({ user: req.user.id }).sort({ createdAt: "desc" });
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
